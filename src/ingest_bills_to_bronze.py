cf4c85cde3b03667a0912ebc03931794eafdcd97
Erik Mendoza <eriik_mendoza@outlook.com>
2026-06-13 20:55:38 -0700

    feat: implement file-based incremental persistence for bills ingestion to prevent RAM saturation

--- src/ingest_bills_to_bronze.py
+++ src/ingest_bills_to_bronze.py
@@ -1,5 +1,6 @@
 import json
 import logging
+import re
 import time
 from datetime import datetime, timezone
 import requests
@@ -12,39 +13,71 @@ BASE_URL = "https://api.congress.gov/v3/bill"
 
 def fetch_bills_data():
     logger.info("Starting data ingestion from Congress API...")
+    limit = 250
+    # offset = 0
+    # all_bills = []
+    has_more_data = True
+    unix_ts = int(time.time())
 
-    query_params = {
-        "api_key": config.congress_api_key.get_secret_value(),
-        "format": "json",
-        "limit": 250,
-    }
     try:
-        logger.info("Requesting data from BASE_URL: %s with limit=250", BASE_URL)
-
-        response = requests.get(BASE_URL, timeout=15, params=query_params)
-        response.raise_for_status()
-
-        data = response.json()
-        bills_count = len(data.get("bills", []))
-        logger.info("Success retrieving %s bills from API", bills_count)
-
         now_utc = datetime.now(timezone.utc)
         current_date = now_utc.strftime("%Y-%m-%d")
         partition_date = f"ingested_at={current_date}"
-        unix_ts = int(time.time())
 
         full_dir_path = config.bronze_path / "bills" / partition_date
         full_dir_path.mkdir(parents=True, exist_ok=True)
 
-        file_name = f"raw_bills_{unix_ts}.json"
-        full_file_path = full_dir_path / file_name
+        existing_offsets = []
 
-        with open(full_file_path, "w", encoding="utf-8") as f:
-            json.dump(data, f, ensure_ascii=False, indent=4)
+        for file in full_dir_path.glob("raw_bills_offset_*.json"):
+            match = re.search(r"raw_bills_offset_(\d+)_", file.name)
+            if match:
+                existing_offsets.append(int(match.group(1)))
+
+        if existing_offsets:
+            last_successful_offset = max(existing_offsets)
+            offset = last_successful_offset + limit
+        else:
+            offset = 0
+
+        while has_more_data:
+            logger.info("Staring pagination with limit=%s and offset=%s", limit, offset)
+
+            query_params = {
+                "api_key": config.congress_api_key.get_secret_value(),
+                "format": "json",
+                "limit": limit,
+                "offset": offset,
+            }
+
+            response = requests.get(BASE_URL, timeout=25, params=query_params)
+            response.raise_for_status()
+            data = response.json()
+
+            current_batch = data.get("bills", [])
+            # Modificacion Edgar veeee esto
+
+            # all_bills.extend(current_batch)
+
+            if current_batch:
+                file_name = f"raw_bills_offset_{offset}_{unix_ts}.json"
+                full_file_path = full_dir_path / file_name
+
+                with open(full_file_path, "w", encoding="utf-8") as f:
+                    json.dump(data, f, ensure_ascii=False, indent=4)
+
+            logger.info(f"Success retrieving {len(current_batch)} bills from API")
+
+            pagination_info = data.get("pagination", {})
+
+            if "next" in pagination_info and len(current_batch) == limit:
+                offset += limit
+            else:
+                has_more_data = False
 
         logger.info("Data ingestion completed successfully")
-
         return full_file_path
 
     except Exception as e:
         logger.error(f"Error fetching bills data: {e}")
+        raise
